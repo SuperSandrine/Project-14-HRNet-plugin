8ee61cd0839469046c6164d6f42d48cde35d8dae
Sandrine Mestas Gleizes <sandrine@Ordinateur-portable-de-Sandrine.local>
2023-05-10 20:32:19 +0200

    package updated

--- dist/components/Modal.js
+++ dist/components/Modal.js
@@ -9,31 +9,31 @@ require("./modal.styles.css");
 var _closeIcon = _interopRequireDefault(require("./../closeIcon.svg"));
 var _SpinnerModal = _interopRequireDefault(require("./SpinnerModal"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
+var _jsxRuntime = require("react/jsx-runtime");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0); } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i.return && (_r = _i.return(), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-/**
- * Creation of a modal component with parameters to personalise some functions
- * @param {Object} props - component props
- * @param {(String|Number|Element)} props.children - add a descriptive paragraph in the modal.
- * @param {Function} props.onClose- function that permit the modal closure by switching false the showModal state.
- * @param {Boolean} props.showModal - Boolean that determines if the modal should be shown or not thanks to parent state
- * @param {String} props.title - Add title to modal
- * @param {Boolean} props.backDropClickAndClose - Boolean that determines if the modal should be closed when the backdrop is clicked
- * @param {Boolean} props.closeAllModalsBefore - Boolean that determines if all modals should be closed before this modal is shown (close only modals created with the same component, in the case of cascade modal)
- * @param {Boolean} props.fadeIn - Boolean that determines if the modal should fade in when shown
- * @param {Number} props.animationDuration - This value should be a number in a string type. The duration of the modal fade-in animation and fade-out in seconds
- * @param {Boolean} props.fadeOut - Boolean that determines if the modal should fade out when closed
- * @param {String} props.dataHref - The URL to fetch data from to render in the modal. URL is provided by the event which turn on the modal, if the event is a click on a tag <a>, then dataHref should be (event.currentTarget.href)
- * @param {string} props.closureButton - Text that appear in the closure button of modal. If it's absent, a default cross button will be displayed instead.
- * @param {String} props.ajaxData - The name of the data to fetch from the URL if the dataHref is an API. Note that it should began with "data.", as "data.name", or "data.phoneNumber".
- * @param {String} props.customButtonColor - The color of the close button and spinner in hexadecimal format, HSL and HSLA format, RGB  and RGBA format and name format
- * @returns The Modal component
- */
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; } /**
+                                                                       * Creation of a modal component with parameters to personalise some functions
+                                                                       * @param {Object} props - component props
+                                                                       * @param {(String|Number|Element)} props.children - add a descriptive paragraph in the modal.
+                                                                       * @param {Function} props.onClose- function that permit the modal closure by switching false the showModal state.
+                                                                       * @param {Boolean} props.showModal - Boolean that determines if the modal should be shown or not thanks to parent state
+                                                                       * @param {String} props.title - Add title to modal
+                                                                       * @param {Boolean} props.backDropClickAndClose - Boolean that determines if the modal should be closed when the backdrop is clicked
+                                                                       * @param {Boolean} props.closeAllModalsBefore - Boolean that determines if all modals should be closed before this modal is shown (close only modals created with the same component, in the case of cascade modal)
+                                                                       * @param {Boolean} props.fadeIn - Boolean that determines if the modal should fade in when shown
+                                                                       * @param {Number} props.animationDuration - This value should be a number in a string type. The duration of the modal fade-in animation and fade-out in seconds
+                                                                       * @param {Boolean} props.fadeOut - Boolean that determines if the modal should fade out when closed
+                                                                       * @param {String} props.dataHref - The URL to fetch data from to render in the modal. URL is provided by the event which turn on the modal, if the event is a click on a tag <a>, then dataHref should be (event.currentTarget.href)
+                                                                       * @param {string} props.closureButton - Text that appear in the closure button of modal. If it's absent, a default cross button will be displayed instead.
+                                                                       * @param {String} props.ajaxData - The name of the data to fetch from the URL if the dataHref is an API. Note that it should began with "data.", as "data.name", or "data.phoneNumber".
+                                                                       * @param {String} props.customButtonColor - The color of the close button and spinner in hexadecimal format, HSL and HSLA format, RGB  and RGBA format and name format
+                                                                       * @returns The Modal component
+                                                                       */
 var Modal = function Modal(props) {
   var modalRef = (0, _react.useRef)(null);
   var _useState = (0, _react.useState)(null),
@@ -166,7 +166,7 @@ var Modal = function Modal(props) {
       setNewDataHref(anchor);
     }
     if (dataHref.includes('#') === false) {
-      setNewDataHref( /*#__PURE__*/React.createElement(_SpinnerModal.default, null));
+      setNewDataHref( /*#__PURE__*/(0, _jsxRuntime.jsx)(_SpinnerModal.default, {}));
       if (!/^data\./.test(ajaxData) | !ajaxData) {
         console.error("No or invalid prop 'ajaxData' supplied to 'Modal'. Value must start with 'data.'");
       } else if (/^data\./.test(ajaxData)) {
@@ -179,10 +179,14 @@ var Modal = function Modal(props) {
         // eslint-disable-next-line no-unused-vars
         .then(function (data) {
           var func = eval("(".concat(ajaxData, ")"));
-          setNewDataHref( /*#__PURE__*/React.createElement("div", null, func));
+          setNewDataHref( /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+            children: func
+          }));
         }).catch(function (error) {
           console.error('Il y a eu un problème avec la requête fetch:', error);
-          setNewDataHref( /*#__PURE__*/React.createElement("div", null, "\"fetch return error\""));
+          setNewDataHref( /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+            children: "\"fetch return error\""
+          }));
         });
       }
     }
@@ -200,50 +204,56 @@ var Modal = function Modal(props) {
       closeButton.click();
     }
   };
-  return showModal ? /*#__PURE__*/React.createElement("div", {
+  return showModal ? /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
     id: "modalBlocker",
     className: "tUv39-modal-blocker-div tUv78",
-    onClick: handleBackDropClick
-  }, /*#__PURE__*/React.createElement("div", {
-    tabIndex: "-1",
-    ref: modalRef,
-    id: "modal",
-    role: "dialog",
-    "aria-labelledby": "dialogTitle",
-    "aria-describedby": "dialogDesc",
-    "aria-modal": "true",
-    onClick: function onClick(e) {
-      return e.stopPropagation();
-    },
-    className: "tUv39-modal-container"
-  }, title && /*#__PURE__*/React.createElement("h3", {
-    className: "tUv39-modalclose-h3",
-    id: "dialogTitle",
-    tabIndex: 1
-  }, title), (children || newDataHref) && /*#__PURE__*/React.createElement("div", {
-    role: "p",
-    className: "tUv39-modalclose-p",
-    id: "dialogDesc",
-    tabIndex: 2
-  }, children, /*#__PURE__*/React.createElement("br", null), newDataHref), closureButton ? /*#__PURE__*/React.createElement("button", {
-    onClick: handleModalClose,
-    id: "dialogCloseButton",
-    className: "tUv39-modalclose-button",
-    "aria-label": "close modal button",
-    tabIndex: 3
-  }, closureButton) : /*#__PURE__*/React.createElement("button", {
-    onClick: handleModalClose,
-    id: "dialogButton",
-    className: "tUv39-modalclose-button-default",
-    "aria-label": "close modal button",
-    tabIndex: 3,
-    style: {
-      customButtonColor: customButtonColor
-    }
-  }, /*#__PURE__*/React.createElement("img", {
-    src: _closeIcon.default,
-    width: '20px'
-  })))) : null;
+    onClick: handleBackDropClick,
+    children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+      tabIndex: "-1",
+      ref: modalRef,
+      id: "modal",
+      role: "dialog",
+      "aria-labelledby": "dialogTitle",
+      "aria-describedby": "dialogDesc",
+      "aria-modal": "true",
+      onClick: function onClick(e) {
+        return e.stopPropagation();
+      },
+      className: "tUv39-modal-container",
+      children: [title && /*#__PURE__*/(0, _jsxRuntime.jsx)("h3", {
+        className: "tUv39-modalclose-h3",
+        id: "dialogTitle",
+        tabIndex: 1,
+        children: title
+      }), (children || newDataHref) && /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+        role: "p",
+        className: "tUv39-modalclose-p",
+        id: "dialogDesc",
+        tabIndex: 2,
+        children: [children, /*#__PURE__*/(0, _jsxRuntime.jsx)("br", {}), newDataHref]
+      }), closureButton ? /*#__PURE__*/(0, _jsxRuntime.jsx)("button", {
+        onClick: handleModalClose,
+        id: "dialogCloseButton",
+        className: "tUv39-modalclose-button",
+        "aria-label": "close modal button",
+        tabIndex: 3,
+        children: closureButton
+      }) : /*#__PURE__*/(0, _jsxRuntime.jsx)("button", {
+        onClick: handleModalClose,
+        id: "dialogButton",
+        className: "tUv39-modalclose-button-default",
+        "aria-label": "close modal button",
+        tabIndex: 3,
+        style: {
+          customButtonColor: customButtonColor
+        },
+        children: /*#__PURE__*/(0, _jsxRuntime.jsx)("img", {
+          src: _closeIcon.default,
+          width: '20px'
+        })
+      })]
+    })
+  }) : null;
 };
 Modal.propTypes = {
   children: _propTypes.default.oneOfType([_propTypes.default.string, _propTypes.default.number, _propTypes.default.element]),
--- dist/components/SpinnerModal.js
+++ dist/components/SpinnerModal.js
@@ -6,17 +6,18 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = void 0;
 var _react = _interopRequireDefault(require("react"));
 require("./spinnerModal.styles.css");
+var _jsxRuntime = require("react/jsx-runtime");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 /**
  * Component displays a spinner in a modal window.
  * @returns {JSX.Element} The SpinnerModal component.
- */
-var SpinnerModal = function SpinnerModal() {
-  return /*#__PURE__*/_react.default.createElement("div", {
-    className: "tUv39-modal-spinner-container"
-  }, /*#__PURE__*/_react.default.createElement("span", {
-    className: "tUv39-modal-spinner"
-  }));
+ */var SpinnerModal = function SpinnerModal() {
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+    className: "tUv39-modal-spinner-container",
+    children: /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+      className: "tUv39-modal-spinner"
+    })
+  });
 };
 var _default = SpinnerModal;
 exports.default = _default;
